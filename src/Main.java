14f37345948abd463a6826a93a8020e1d9387df7
Roger Yamassaki <135734775+Skarefes@users.noreply.github.com>
2024-07-15 15:03:42 -0300

    Update Main.java
    mudanças

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,1 @@
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-    Scanner t = new Scanner(System.in);
-        System.out.println("Em que ano vc nasceu? ");
-    int nasc = t.nextInt();
-    int i = 2024-nasc;
-        System.out.println("Sua idade é " + i);
-    if (i>=18){
-        System.out.println("Voce é maior de idade");
-    }else {
-        System.out.println("Voce é menor de idade");
-    }
-    }
-}
+
